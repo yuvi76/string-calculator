fd08789cc906ac07f4c07fe877b0520affaf0c65
yuvraj-bytes <upadhyayyuvi@gmail.com>
2025-03-20 20:24:22 +0530

    Test: add test for single number

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  it("should return the number itself when only one number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
 });
